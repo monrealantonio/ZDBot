6a271de2cb16e18baec83dc82c0717cfd3cc11d4
Antonio Monreal <monrealantonio@gmail.com>
2019-10-12 18:06:29 -0500

    Added ability to update secret roles on the Discord server.

--- ZDBot.py
+++ ZDBot.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+BOT_PREFIX = ("!")
 
 client = discord.Client()
 
@@ -20,14 +21,38 @@ async def on_ready():
             f'{guild.name}(id: {guild.id})\n'
         )
 
-# !roll command
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author.bot:
         return
-    roll = random.randint(1,21)
-    if message.content == '!roll':
-        response = f'*{message.author.name} has rolled a(n) {roll}. {message.author.mention}*'
-        await message.channel.send(response)
+    elif message.content[0] == BOT_PREFIX:
+        if message.content == '!roll':
+            await on_roll(message)
+        elif message.content.split(" ")[0] == '!secret':
+            await message.delete()
+            print(f'Message from {message.author.name} removed for... secret reasons.\n')
+            await on_secret(message)
+
+
+# !roll - Event for users wanting to roll
+@client.event
+async def on_roll(message):
+    roll = random.randint(1,20)
+    response = f'*{message.author.mention} has rolled a {roll}.*\n'
+    await message.channel.send(response)
+    print(f'{message.author.name} has rolled a {roll}.\n')
+
+# !secret commands that update roles.
+async def on_secret(message):
+    id = client.get_guild(468206684834365441)
+    secretRole = message.content.split(" ")[1]
+    if secretRole == 'cheese-banana':
+        role = discord.utils.get(id.roles, id=628819307853512705)
+        await message.author.add_roles(role)
+        print(f'{message.author.name} has become a Harlot!\n')
+    elif secretRole == 'movietime':
+        role = discord.utils.get(id.roles, id=631995695561441280)
+        await message.author.add_roles(role)
+        print(f'{message.author.name} is watching Movies!\n')
 
 client.run(TOKEN)
